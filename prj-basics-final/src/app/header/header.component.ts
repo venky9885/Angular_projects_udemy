40d4e7bca765d0da7d0f95297176875c7f7f90f6
Venkatesh <59081163+venky9885@users.noreply.github.com>
2023-01-07 15:09:18 +0530

    1.0.1

---
+++ prj-basics-final/src/app/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+  @Output() featureSelected = new EventEmitter<string>();
+
+  onSelect(feature: string) {
+    console.log(feature);
+    this.featureSelected.emit(feature);
+    // if (feature === 'recipe') {
+
+    // } else {
+
+    // }
+  }
+}
